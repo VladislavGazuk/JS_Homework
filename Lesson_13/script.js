e55061ec066c39fa9be13cecebf17192a8ac9fb4
Владислав Газук <vladislav.gazuk@gmail.com>
2020-07-20 23:12:23 +0300

    Homework lesson 13 (corrected)

--- Lesson_13/script.js
+++ Lesson_13/script.js
@@ -1,47 +1,64 @@
 //Задание №1
 
-var name = ['Vasya'];
-var newArr = name.map(function () {
-    return 'name: ' + name;
-});
+function name(arr) {
+    var newArr = [];
+
+    arr.map(function (item,i){
+        newArr[i] = new Object(item)
+    })
+
+    return 'name: ' + newArr;
+}
+
+name(['Vasya']);
 
 
 //Задание №2
 
-var arr = ['00', '13', '24'];
-var time = arr.reduce(function (accumulator, currentValue, index) {
-    return (index !== 2) ? accumulator + currentValue + ' : ' : accumulator + currentValue;
-}, 'Текущее время : ');
+function getTime(arr) {
+    var time = arr.reduce(function (accumulator, currentValue) {
+        return accumulator + ' : ' + currentValue;
+    }, 'Текущее время ');
+
+    return time;
+}
+
+getTime(['00', '13', '24']);
 
 
 //Задание №3
 
-var vowel = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+function getWord(text) {
+    var vowelLetters = ['а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я'];
+    var number = 0;
+    text.toLowerCase().split('').reduce(function (n, c){
+        return vowelLetters.indexOf(c) > -1 ? ++number : number;
+    },0)
+    return number;
+}
 
-var vowelLength = [..."Африканец"].reduce(function (n, c) {
-    return vowel.indexOf(c) > -1 ? ++n : n;
-}, 0);
+getWord('Африканец');
 
 
 //Задание №4
 
 function countSentencesLetters(sentence) {
-    var arr = sentence.split(/[\.\?\!]/);
+    var sentenceSplit = sentence.split(/[!?...]+/);
 
-    console.log(arr[0] + ': ' + 'Letters quantity is: ' + firstLength)
-    console.log(second + ': ' + 'Letters quantity is: ' + secondLength)
-    console.log(arr[4] + ': ' + 'Letters quantity is: ' + thirdLength)
+    function totalLetters(key) {
+        var sum=0;
+
+        for( var v in key.split(/[,\s]/g).join('')){
+            sum++
+        }
+
+        return sum;
+    }
+
+    sentenceSplit.forEach(function (item) {
+        if (item.length != 0)
+            console.log(item.trim(), ': Letters quantity is:', totalLetters(item))
+    });
 }
 
-var sentence = 'Привет, студент! Студент... Как дела, студент?';
-
-var first = arr[0].replace(/[\s.,]/g, ''),
-    firstLength = first.length;
-
-var second = arr[1].replace(/[\s.,]/g, ''),
-    secondLength = second.length;
-
-var third = arr[4].replace(/[\s.,]/g, ''),
-    thirdLength = third.length;
-
-countSentencesLetters(sentence);
+countSentencesLetters('Привет, студент! Студент... Как дела, студент?');
